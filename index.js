ae541745f6230dfbc971f3f08fb1ea597c24f294
Dimpy khatwani <134204043+Dev-traning@users.noreply.github.com>
2024-04-22 17:01:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.json([{
+            id: 1,
+            name: "dimpy",
+            age: 21
+        },
+
+
+    ])
+});
+app.listen(55000, () => {
+    console.log("As it is working on  5500 server ")
+})
